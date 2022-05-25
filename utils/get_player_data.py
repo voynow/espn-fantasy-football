c13a8186696500a15aadb2ec53d89fac001651bd
voynow <voynow99@gmail.com>
2022-05-25 15:12:27 -0400

    Serious progress - not fully functioning though

--- utils/get_player_data.py
+++ utils/get_player_data.py
@@ -1,10 +1,9 @@
-from locale import normalize
 from selenium import webdriver
-from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 
 import time
@@ -38,11 +37,36 @@ def navigate_to_scoring_leaders(driver):
 def get_player_links(driver):
 
     player_links = driver.find_elements(By.CLASS_NAME, "link")
-    player_links[0].click()
-    player_card = driver.find_element(By.CLASS_NAME, "player-card-center")
-    # player_card = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.CLASS_NAME, "player-card-center")))
-    # player_card = player_links[0].find_element(By.CLASS_NAME, "player-card-center")
-    print(player_card.text)
+
+    data_collection = []
+    for link in player_links:
+
+        link.click()
+        player_card = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.CLASS_NAME, "player-card-center")))
+        player_card.find_element(By.CLASS_NAME, "header_link").click()
+
+        new_window = driver.window_handles[-1]
+        old_window = driver.window_handles[-2]
+        driver.switch_to.window(new_window)
+
+        nav_elements = driver.find_elements(By.CLASS_NAME, "Nav__Text")
+        for element in nav_elements:
+            if element.text == "Game Log":
+                element.click()
+                break
+
+        player_data_2021 = []
+        data_tables = driver.find_elements(By.CLASS_NAME, "mb4")
+        for table in data_tables:
+            player_data_2021.append(table.text)
+        data_collection.append(player_data_2021)
+
+        # driver.send_keys(Keys.ARROW_DOWN + "w")
+        driver.close()
+        driver.switch_to.window(old_window)
+        driver.find_element(By.CLASS_NAME, "lightbox__closebtn").click()
+
+    return data_collection
 
 
 def next_page(driver):
@@ -71,7 +95,6 @@ def extract():
     next_page_exists = True
     while next_page_exists:
         get_player_links(driver)
-        1/0
-        # next_page_exists = next_page(driver)
+        next_page_exists = next_page(driver)
         
     return player_data
