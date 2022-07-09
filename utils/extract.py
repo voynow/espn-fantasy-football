2071d4a999e4241748f6b72e6a4581c225313cb8
voynow <voynow99@gmail.com>
2022-07-09 10:52:55 -0400

    Update window switch to driver create/delete

--- utils/extract.py
+++ utils/extract.py
@@ -1,3 +1,4 @@
+from lib2to3.pgen2 import driver
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
@@ -8,12 +9,12 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 
-def create_driver():
+def create_driver(link):
     """
-    Create driver and load espn fantasy football page
+    Create chrome driver given web link
     """
     driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.get("https://fantasy.espn.com/football/leaders")
+    driver.get(link)
 
     return driver
 
@@ -28,36 +29,26 @@ def get_player_links(driver):
     return player_links
 
 
-def access_player_card(driver, link):
+def get_player_metadata(driver, link):
     """
     Open player card and click on player's "complete stats"
     """
     player_name = link.text
-    print(player_name)
-    print(f"Link: {link}")
+    print(f"{player_name}/nLink: {link}")
     link.click()
+
     try:
-        player_card = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "player-card-center")))
+        player_card = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "player-card-center")))
         print(f"Player card: {player_card}")
-    except:
-        print(f"ERROR: skipping {player_name} - could not find class_name=player-card-center")
-        return False
-    player_card.find_element(By.CLASS_NAME, "header_link").click()
-    return player_name
+    except BaseException as e:
+        print(e)
+        print(f"ERROR cannot find {player_name}'s player card")
+        return player_name, None
 
+    complete_stats_obj = player_card.find_element(By.CLASS_NAME, "header_link")
+    complete_stats_link = complete_stats_obj.get_attribute('href')
 
-def switch_to_new_window(driver):
-    """
-    Update driver window to most recent window
-    """
-    new_window = driver.window_handles[-1]
-    print(f"New window: {new_window}")
-    if base_window is None:
-        base_window = driver.window_handles[-2]
-    print(f"Base window: {base_window}")
-    driver.switch_to.window(new_window)
-
-    return driver, base_window
+    return player_name, complete_stats_link
 
 
 def access_game_log(driver):
@@ -67,8 +58,11 @@ def access_game_log(driver):
     nav_elements = driver.find_elements(By.CLASS_NAME, "Nav__Text")
     for element in nav_elements:
         if element.text == "Game Log":
-            element.click()
-            break
+            try:
+                element.click()
+                break
+            except selenium.common.exceptions.WebDriverException:
+                return None
     time.sleep(.25)
 
     return driver
@@ -85,16 +79,6 @@ def collect_data(driver, player_name):
         print(f"{i} {table}")
         game_level_data.append(table.text)
     return game_level_data
-
-
-def switch_to_base_window(driver, base_window):
-    """
-    Update driver window to original window
-    """
-    driver.close()
-    driver.switch_to.window(base_window)
-
-    return driver
 
 
 def next_page(driver):
@@ -116,25 +100,30 @@ def extract():
     Execute data collection processes for 2021/2022 espn fantasy football data
     Data containing season level statistics for all players
     """
-    driver = create_driver()
+    espn_ff_scoring_laders_link = "https://fantasy.espn.com/football/leaders"
+    driver = create_driver(espn_ff_scoring_laders_link)
     player_links = get_player_links(driver)
 
-    base_window = None
+    missing_players = []
     data_collection = []
-    for link in player_links:
+    for player_link in player_links:
         
-        player_name = access_player_card(driver, link)
-        if not player_name:
-            continue
+        player_name, complete_stats_link = get_player_metadata(driver, player_link)
+        if not complete_stats_link:
+            missing_players.append(player_name)
 
-        driver, base_window = switch_to_new_window(driver)
-        driver = access_game_log(driver)
-        player_data = collect_data(driver, player_name)
-        data_collection.append(player_data)
-        driver = switch_to_base_window(driver, base_window)
+        player_driver = create_driver(complete_stats_link)
+        player_driver = access_game_log(player_driver)
+        if player_driver:
+            player_data = collect_data(player_driver, player_name)
+            data_collection.append(player_data)
+        else:
+            missing_players.append(player_name)
+
+        player_driver.quit()
         driver.find_element(By.CLASS_NAME, "lightbox__closebtn").click()
 
-    return data_collection
+    return data_collection, missing_players
 
 
     # player_data = []
